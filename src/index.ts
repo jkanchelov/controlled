2bdfd83dfae8ddcf3e1352481ac2b39a5fa20988
jkanchelov <jkanchelov@gmail.com>
2021-03-31 16:17:41 +0300

    refactor: temp connect to localhost server

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import startupService from "./startupService";
 
 import axios from "axios";
 (async (): Promise<void> => {
-    const { data: serverAddress } = await axios.get("https://control-js.azurewebsites.net/api/getServerAddress");
+    // const { data: serverAddress } = await axios.get("https://control-js.azurewebsites.net/api/getServerAddress")
+    const serverAddress = "ws://localhost";
 
     const wsSession = new WSSessionKeeper(serverAddress);
     const commandHandler = new CommandHandler(wsSession.ws);
 
     wsSession.addSubscriber(commandHandler);
 
-    await startupService();
+    // await startupService();
 })();
